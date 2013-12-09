ced1c6cd03aa068ec2dc2ddbc78bbbd9fd4ff0ab
James Willcox <snorp@snorp.net>
2013-12-09 13:30:09 -0600

    Single GLContext
    Conflicts:
    	gfx/2d/DrawTargetSkia.cpp

--- gfx/gl/SurfaceStream.cpp
+++ gfx/gl/SurfaceStream.cpp
@@ -7,6 +7,7 @@
 
 #include "gfxPoint.h"
 #include "SharedSurface.h"
+#include "SharedSurfaceGL.h"
 #include "SurfaceFactory.h"
 #include "GeckoProfiler.h"
 
@@ -54,6 +55,14 @@ SurfaceStream::CreateForType(SurfaceStreamType type, mozilla::gl::GLContext* glC
 
     result->mGLContext = glContext;
     return result;
+}
+
+void
+SurfaceStream_TripleBuffer::CopySurfaceToProducer(SharedSurface* src, SurfaceFactory* factory)
+{
+    mProducer->LockProdImpl();
+    SharedSurface::Copy(src, mProducer, factory);
+    mProducer->UnlockProdImpl();
 }
 
 void
@@ -179,6 +188,25 @@ SurfaceStream::Resize(SurfaceFactory* factory, const gfxIntSize& size)
     }
 
     New(factory, size, mProducer);
+    return mProducer;
+}
+
+SharedSurface*
+SurfaceStream_TripleBuffer::Resize(SurfaceFactory* factory, const gfxIntSize& size)
+{
+    MonitorAutoLock lock(mMonitor);
+
+    if (mProducer) {
+        Scrap(mProducer);
+    }
+
+    if (mStaging) {
+        Scrap(mStaging);
+    }
+
+    New(factory, size, mProducer);
+    New(factory, size, mStaging);
+
     return mProducer;
 }
 
