92ad8bbbcd9055273db8466acfe6b92f96fca508
James Willcox <snorp@snorp.net>
2014-10-02 14:07:13 -0500

    Bug 1014614 - Do not try to use a temporary texture for SurfaceTexture

--- gfx/layers/opengl/TextureHostOGL.cpp
+++ gfx/layers/opengl/TextureHostOGL.cpp
@@ -600,10 +600,8 @@ SurfaceTextureSource::BindTexture(GLenum aTextureUnit, gfx::Filter aFilter)
     NS_WARNING("Trying to bind a texture without a GLContext");
     return;
   }
-  GLuint tex = mCompositor->GetTemporaryTexture(GetTextureTarget(), aTextureUnit);
 
   gl()->fActiveTexture(aTextureUnit);
-  gl()->fBindTexture(mTextureTarget, tex);
 #ifndef DEBUG
   // SurfaceTexture spams us if there are any existing GL errors, so
   // we'll clear them here in order to avoid that.
@@ -636,6 +634,7 @@ gfx::Matrix4x4
 SurfaceTextureSource::GetTextureTransform()
 {
   gfx::Matrix4x4 ret;
+ 
   mSurfTex->GetTransformMatrix(ret);
 
   return ret;
